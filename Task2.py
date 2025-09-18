9d52cb6ed4a9e4af244da5978716b480ee0f9857
ronit-s-cgs <s30358@caseygrammar.vic.edu.au>
2025-09-18 01:38:25 +0000

    Final Version Possibly

--- Task2.py
+++ Task2.py
@@ -1,8 +1,8 @@
 # Task 2: James Bond
 def main():
-    #Write your code for input here
-    
-
+   first = input("What is your first name? ")
+   last = input("What is your last name? ")
+   print(f"My name is {last}, {first} {last}")
 
     # End of your code for input here
 
